780dec62d336060b8e67de6c344dde58f82a8464
myst3l <karakumayl@gmail.com>
2023-05-09 18:54:22 -0400

    from pc

--- alreadyOpen.py
+++ alreadyOpen.py
@@ -4,3 +4,5 @@ import time
 
 # change url of chat-box to add name of current open stream
 # or pop-out chat
+
+
--- openNew.py
+++ openNew.py
@@ -2,17 +2,14 @@ import subprocess
 import pyautogui
 import time
 
-url1 = 'https://www.twitch.tv/xqc'
+url1 = 'https://www.twitch.tv/forsen'
 url2 = 'https://www.twitch.tv/popout/xqc/chat?popout='
-#
+
+# -------------------------------------------------------------------
+# STREAM WINDOW
 subprocess.Popen(['C:/Program Files/Google/Chrome/Application/chrome.exe', url1, '--new-window'])
-subprocess.Popen(['C:/Program Files/Google/Chrome/Application/chrome.exe', url2, '--new-window'])
-
-time.sleep(1)                                                   # buffer of 1 second to allow page titles to load
-
-# MOVE, RESIZE, CLOSE CHAT AND SET TO THEATRE MODE
-
-window1 = pyautogui.getWindowsWithTitle('xQc - Twitch')[0]
+time.sleep(0.4)
+window1 = pyautogui.getWindowsWithTitle('Google Chrome')[0]
 
 window1.moveTo(100, 100)                                        # To get around window opening on second monitor
 window1.moveTo(1912, -250)
@@ -23,11 +20,25 @@ pyautogui.dragRel(0, -100, duration=0.4)                        # To remove whit
 window1.moveTo(1912, -250)
 window1.resizeTo(1096, 685)
 
-time.sleep(0.1)
+# MOVE, RESIZE, CLOSE CHAT AND SET TO THEATRE MODE
+
+pyautogui.leftClick(x=2300, y=100)                                 # Bring window in focus
+pyautogui.hotkey('alt', 't', 'r')                                       # Theatre Mode keyboard
+# pyautogui.hotkey('alt', 'r')                                       # Collapse Chat keyboard
+
+# Mouse emulation
+# time.sleep(0.04)
+# pyautogui.leftClick(x=2644, y=166)                                  # Theatre Mode
+# time.sleep(0.04)
+# pyautogui.leftClick(x=2716, y=-150)                                 # Collapse Chat
+# -------------------------------------------------------------------
 
 # CHAT WINDOW
+subprocess.Popen(['C:/Program Files/Google/Chrome/Application/chrome.exe', url2, '--new-window'])
 
-window2 = pyautogui.getWindowsWithTitle('Chat')[0]
+time.sleep(0.3)                                 # buffer of 1 second to allow Chrome to load
+
+window2 = pyautogui.getWindowsWithTitle('Google Chrome')[0]
 
 window2.moveTo(100, 100)                                            # To get around window opening on second monitor
 window2.moveTo(1912, 427)
@@ -39,11 +50,3 @@ window2.resizeTo(1096, 800)
 
 # -------------------------------------------------------------------
 
-# pyautogui.leftClick(x=2300, y=100)
-# pyautogui.hotkey('alt', 't')                                       # Theatre Mode keyboard
-# pyautogui.hotkey('alt', 'r')                                       # Collapse Chat keyboard
-
-# time.sleep(0.04)
-# pyautogui.leftClick(x=2644, y=166)                                  # Theatre Mode
-# time.sleep(0.04)
-# pyautogui.leftClick(x=2716, y=-150)                                 # Collapse Chat
